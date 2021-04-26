3cda0a070fedea7627c0ce67899dd6c45b82e3db
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-04-26 16:45:39 -0400

    continue with fine-tuning

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//DOM Element
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.menu-items');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    menuItems.classList.toggle('open');
+})
